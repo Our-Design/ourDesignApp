42611bd1d4af15c1d03bf8da146b0254fde86925
Aman Kumar <amankum@ICS-LAP-11046.local>
2025-04-19 22:49:10 +0530

    add: android splash screen

--- App.tsx
+++ App.tsx
@@ -1,25 +1,24 @@
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {Provider} from 'react-redux';
+import {store} from './src/store';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppProvider from './src/AppProvider';
 import UILayer from './src/components/UILayer';
 import Toast from 'react-native-toast-message';
-
-
+import {useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 
 export default function App() {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true}); // Hide splash screen when ready
+  }, []);
 
   return (
-      <SafeAreaProvider>
-        <Provider store={store}>
+    <SafeAreaProvider>
+      <Provider store={store}>
         <UILayer />
-        <AppProvider/>
-        </Provider>
-        <Toast
-          topOffset={50}
-          visibilityTime={3000}
-          position="top"
-        />
-      </SafeAreaProvider>
+        <AppProvider />
+      </Provider>
+      <Toast topOffset={50} visibilityTime={3000} position="top" />
+    </SafeAreaProvider>
   );
 }
